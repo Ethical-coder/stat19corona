7700b410c1b56ef911d7403e268b338e0244027e
coder_iota <akshat732000@gmail.com>
2020-04-29 18:40:18 +0530

    Designed the basic UI

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import pygal 
+import pickle
+
 from flask import Flask, render_template
 
 app = Flask(__name__, template_folder = 'template')
@@ -8,7 +11,10 @@ def home():
 
 @app.route('/graph')
 def plot():
-    return render_template("graph.html")
+    response = [None, None]
+    response[0] = pickle.load( open('./plots/MP_Corona_Cases.sav', 'rb') )
+    response[1] = pickle.load( open('./plots/MP_Districts_pie_chart.sav', 'rb') )
+    return render_template("graph.html", response = response)
 
 if __name__ == '__main__':
     app.run(debug=True)
